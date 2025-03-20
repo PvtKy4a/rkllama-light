1386299683db69c0c1f37567b1dc4bd901c657ca
Vladislav Pavlov <wladpav@yandex.ru>
2025-03-20 16:24:28 +0300

    refactor

--- model_run.py
+++ model_run.py
@@ -2,22 +2,22 @@ import os
 import sys
 import signal
 import readline
-from rkllm import RKLLM
+from rkllm import model
 from model_utils import download_tokenizer, download_model
 
 def print_help():
-    print("/regenerate      Regenerate last response", flush=True)
-    print("/clear           Clear chat history", flush=True)
-    print("/set history     Enable chat history (enabled by default)", flush=True)
-    print("/unset history   Disable chat history", flush=True)
-    print("/save <file>     Save chat history to specified file", flush=True)
-    print("/load <file>     Load chat history from specified file", flush=True)
-    print("/bye             Exit", flush=True)
-    print(flush=True)
-    print("Ctrl + l         Clear the screen", flush=True)
-    print("Ctrl + c         Stop the model from responding", flush=True)
-    print("Ctrl + d         Exit (/bye)", flush=True)
-    print(flush=True)
+    print("/regenerate      Regenerate last response")
+    print("/clear           Clear chat history")
+    print("/set history     Enable chat history (enabled by default)")
+    print("/unset history   Disable chat history")
+    print("/save <file>     Save chat history to specified file")
+    print("/load <file>     Load chat history from specified file")
+    print("/bye             Exit")
+    print()
+    print("Ctrl + l         Clear the screen")
+    print("Ctrl + c         Stop the model from responding")
+    print("Ctrl + d         Exit (/bye)")
+    sys.stdout.flush()
 
 def model_run(model_name):
     LIB_PATH = "./lib/librkllmrt.so"
@@ -36,11 +36,10 @@ def model_run(model_name):
         print("\nIncorrect model filename.", flush=True)
         return
 
-    rkllm_model = RKLLM(LIB_PATH, MODELS_PATH, model_name)
+    rkllm_model = model(LIB_PATH, MODELS_PATH, model_name)
 
     def abort_handler(sig, frame):
         rkllm_model.set_abort()
-        rkllm_model.rkllm_abort(rkllm_model.rkllm_handle)
 
     default_sigint_handler = signal.getsignal(signal.SIGINT)
 
@@ -92,14 +91,14 @@ def model_run(model_name):
             continue
 
         if user_input == "/clear":
-            rkllm_model.clear_history()
+            rkllm_model.history_clear()
             continue
 
         signal.signal(signal.SIGINT, abort_handler)
 
         print("\nAI: ", flush=True, end="")
 
-        rkllm_model.RKLLM_run(user_input, regenerate=regenerate, enable_history=enable_history)
+        rkllm_model.run(user_input, regenerate=regenerate, enable_history=enable_history)
 
         regenerate = False
 
@@ -107,4 +106,4 @@ def model_run(model_name):
 
     print("\nExiting...", flush=True)
 
-    rkllm_model.rkllm_destroy(rkllm_model.rkllm_handle)
+    rkllm_model.destroy()
--- rkllm.py
+++ rkllm.py
@@ -10,107 +10,135 @@ def rkllm_cb_imp(result, userdata, state):
     rkllm_model = ctypes.cast(userdata, ctypes.py_object).value
 
     if state == rktypes.LLMCallState.RKLLM_RUN_NORMAL:
-        token = rkllm_model.tokenizer.decode(result.contents.token_id, skip_special_tokens=True)
-        rkllm_model.response += token
+        token:str = rkllm_model.decode(result.contents.token_id, skip_special_tokens=True)
+        rkllm_model.response_append(token)
         print(token, flush=True, end="")
     elif state == rktypes.LLMCallState.RKLLM_RUN_FINISH:
         print("", flush=True)
-        if rkllm_model.abort:
-            rkllm_model.abort = False
+        if rkllm_model.is_abort():
+            rkllm_model.reset_abort()
         else:
-            rkllm_model.history.append({"role": "assistant", "content": rkllm_model.response})
+            rkllm_model.finish()
     elif state == rktypes.LLMCallState.RKLLM_RUN_ERROR:
         print("RKLLM run error", flush=True)
     else:
         pass
 
-class RKLLM:
+class model:
     def __init__(self, lib_path, models_path, model_name):
-        self.rkllm_lib = ctypes.CDLL(lib_path)
-        self.model = get_model(model_name)
-        self.tokenizer = get_tokenizer(models_path, self.model)
-        self.rkllm_cb = rkllm_cb_type(rkllm_cb_imp)
-        self.history = []
-        self.response = ""
-        self.abort = False
+        self.__rkllm_lib = ctypes.CDLL(lib_path)
+        self.__model = get_model(model_name)
+        self.__tokenizer = get_tokenizer(models_path, self.__model)
+        self.__rkllm_cb = rkllm_cb_type(rkllm_cb_imp)
+        self.__history = []
+        self.__response = ""
+        self.__abort_flag = False
 
-        self.rkllm_param = rktypes.RKLLMParam()
-        self.rkllm_param.model_path = bytes(models_path + "/" + self.model["filename"], 'utf-8')
-        self.rkllm_param.max_context_len = self.model["max_context_len"]
-        self.rkllm_param.max_new_tokens = self.model["max_new_tokens"]
-        self.rkllm_param.skip_special_token = True
-        self.rkllm_param.top_k = self.model["top_k"]
-        self.rkllm_param.top_p = self.model["top_p"]
-        self.rkllm_param.temperature = self.model["temperature"]
-        self.rkllm_param.repeat_penalty = self.model["repeat_penalty"]
-        self.rkllm_param.frequency_penalty = self.model["frequency_penalty"]
-        self.rkllm_param.presence_penalty = self.model["presence_penalty"]
-        self.rkllm_param.mirostat = self.model["mirostat"]
-        self.rkllm_param.mirostat_tau = self.model["mirostat_tau"]
-        self.rkllm_param.mirostat_eta = self.model["mirostat_eta"]
-        self.rkllm_param.is_async = False
-        self.rkllm_param.img_start = "".encode('utf-8')
-        self.rkllm_param.img_end = "".encode('utf-8')
-        self.rkllm_param.img_content = "".encode('utf-8')
-        self.rkllm_param.extend_param.base_domain_id = 0
+        self.__rkllm_param = rktypes.RKLLMParam()
+        self.__rkllm_param.model_path = bytes(models_path + "/" + self.__model["filename"], 'utf-8')
+        self.__rkllm_param.max_context_len = self.__model["max_context_len"]
+        self.__rkllm_param.max_new_tokens = self.__model["max_new_tokens"]
+        self.__rkllm_param.skip_special_token = True
+        self.__rkllm_param.top_k = self.__model["top_k"]
+        self.__rkllm_param.top_p = self.__model["top_p"]
+        self.__rkllm_param.temperature = self.__model["temperature"]
+        self.__rkllm_param.repeat_penalty = self.__model["repeat_penalty"]
+        self.__rkllm_param.frequency_penalty = self.__model["frequency_penalty"]
+        self.__rkllm_param.presence_penalty = self.__model["presence_penalty"]
+        self.__rkllm_param.mirostat = self.__model["mirostat"]
+        self.__rkllm_param.mirostat_tau = self.__model["mirostat_tau"]
+        self.__rkllm_param.mirostat_eta = self.__model["mirostat_eta"]
+        self.__rkllm_param.is_async = False
+        self.__rkllm_param.img_start = "".encode('utf-8')
+        self.__rkllm_param.img_end = "".encode('utf-8')
+        self.__rkllm_param.img_content = "".encode('utf-8')
+        self.__rkllm_param.extend_param.base_domain_id = 0
 
-        self.rkllm_handle = rktypes.RKLLM_Handle_t()
-        self.rkllm_init = self.rkllm_lib.rkllm_init
-        self.rkllm_init.restype = ctypes.c_int
-        self.rkllm_init.argtypes = [ctypes.POINTER(rktypes.RKLLM_Handle_t), ctypes.POINTER(rktypes.RKLLMParam), rkllm_cb_type]
-        self.rkllm_init(self.rkllm_handle, self.rkllm_param, self.rkllm_cb)
+        self.__rkllm_handle = rktypes.RKLLM_Handle_t()
+        self.__rkllm_init = self.__rkllm_lib.rkllm_init
+        self.__rkllm_init.restype = ctypes.c_int
+        self.__rkllm_init.argtypes = [ctypes.POINTER(rktypes.RKLLM_Handle_t), ctypes.POINTER(rktypes.RKLLMParam), rkllm_cb_type]
+        self.__rkllm_init(self.__rkllm_handle, self.__rkllm_param, self.__rkllm_cb)
 
-        self.rkllm_infer_params = rktypes.RKLLMInferParam()
-        ctypes.memset(ctypes.byref(self.rkllm_infer_params), 0, ctypes.sizeof(rktypes.RKLLMInferParam))
-        self.rkllm_infer_params.mode = rktypes.RKLLMInferMode.RKLLM_INFER_GENERATE
+        self.__rkllm_infer_params = rktypes.RKLLMInferParam()
+        ctypes.memset(ctypes.byref(self.__rkllm_infer_params), 0, ctypes.sizeof(rktypes.RKLLMInferParam))
+        self.__rkllm_infer_params.mode = rktypes.RKLLMInferMode.RKLLM_INFER_GENERATE
 
-        self.rkllm_input = rktypes.RKLLMInput()
-        self.rkllm_input.input_mode = rktypes.RKLLMInputMode.RKLLM_INPUT_TOKEN
+        self.__rkllm_input = rktypes.RKLLMInput()
+        self.__rkllm_input.input_mode = rktypes.RKLLMInputMode.RKLLM_INPUT_TOKEN
 
-        self.rkllm_run = self.rkllm_lib.rkllm_run
-        self.rkllm_run.restype = ctypes.c_int
-        self.rkllm_run.argtypes = [rktypes.RKLLM_Handle_t, ctypes.POINTER(rktypes.RKLLMInput), ctypes.POINTER(rktypes.RKLLMInferParam), ctypes.py_object]
+        self.__rkllm_run = self.__rkllm_lib.rkllm_run
+        self.__rkllm_run.restype = ctypes.c_int
+        self.__rkllm_run.argtypes = [rktypes.RKLLM_Handle_t, ctypes.POINTER(rktypes.RKLLMInput), ctypes.POINTER(rktypes.RKLLMInferParam), ctypes.py_object]
 
-        self.rkllm_abort = self.rkllm_lib.rkllm_abort
-        self.rkllm_abort.restype = ctypes.c_int
-        self.rkllm_abort.argtypes = [rktypes.RKLLM_Handle_t]
+        self.__rkllm_abort = self.__rkllm_lib.rkllm_abort
+        self.__rkllm_abort.restype = ctypes.c_int
+        self.__rkllm_abort.argtypes = [rktypes.RKLLM_Handle_t]
 
-        self.rkllm_destroy = self.rkllm_lib.rkllm_destroy
-        self.rkllm_destroy.argtypes = [rktypes.RKLLM_Handle_t]
-        self.rkllm_destroy.restype = ctypes.c_int
+        self.__rkllm_destroy = self.__rkllm_lib.rkllm_destroy
+        self.__rkllm_destroy.argtypes = [rktypes.RKLLM_Handle_t]
+        self.__rkllm_destroy.restype = ctypes.c_int
 
-    def list_to_ctype_array(self, tokens, ctype):
+    def get_history_len(self):
+        return len(self.__history)
+    
+    def history_append(self, data):
+        self.__history.append(data)
+
+    def history_pop(self, index=-1):
+        return self.__history.pop(index)
+
+    def history_clear(self):
+        self.__history.clear()
+
+    def response_append(self, data):
+        self.__response += data
+
+    def response_clear(self):
+        self.__response = ""
+
+    def set_abort(self):
+        self.__abort_flag = True
+        self.__rkllm_abort(self.__rkllm_handle)
+        self.history_pop()
+
+    def reset_abort(self):
+        self.__abort_flag = False
+
+    def is_abort(self):
+        return self.__abort_flag
+
+    def __list_to_ctype_array(self, tokens, ctype):
         # Converts a Python list to a ctype array.
         return (ctype * len(tokens))(*tokens)
-    
-    def get_history_len(self):
-        return len(self.history)
-    
-    def clear_history(self):
-        self.history.clear()
-    
-    def set_abort(self):
-        self.abort = True
 
-    # Retrieve the output from the RKLLM model and print it in a streaming manner
-    def RKLLM_run(self, request, regenerate=False, enable_history=True):
+    def run(self, request, regenerate=False, enable_history=True):
         if not enable_history:
-            self.clear_history()
+            self.history_clear()
 
-        if not self.model["system_prompt"] == "":
-            self.history.append({"role": "system", "content": self.model["system_prompt"]})
+        if not self.__model["system_prompt"] == "":
+            self.history_append({"role": "system", "content": self.__model["system_prompt"]})
 
         if regenerate:
-            self.history.pop()
+            self.history_pop()
         else:
-            self.history.append({"role": "user", "content": request})
+            self.history_append({"role": "user", "content": request})
 
-        tokens = self.tokenizer.apply_chat_template(self.history, tokenize=True, add_generation_prompt=True) # The tokenizer outputs as a Python list.
+        tokens = self.__tokenizer.apply_chat_template(self.__history, tokenize=True, add_generation_prompt=True) # The tokenizer outputs as a Python list.
 
-        self.response = ""
+        self.response_clear()
 
-        self.rkllm_input.input_data.token_input.input_ids = self.list_to_ctype_array(tokens, ctypes.c_int)
+        self.__rkllm_input.input_data.token_input.input_ids = self.__list_to_ctype_array(tokens, ctypes.c_int)
 
-        self.rkllm_input.input_data.token_input.n_tokens = ctypes.c_ulong(len(tokens))
+        self.__rkllm_input.input_data.token_input.n_tokens = ctypes.c_ulong(len(tokens))
 
-        self.rkllm_run(self.rkllm_handle, ctypes.byref(self.rkllm_input), ctypes.byref(self.rkllm_infer_params), ctypes.py_object(self))
+        self.__rkllm_run(self.__rkllm_handle, ctypes.byref(self.__rkllm_input), ctypes.byref(self.__rkllm_infer_params), ctypes.py_object(self))
+
+    def finish(self):
+        self.history_append({"role": "assistant", "content": self.__response})
+
+    def decode(self, token_id, skip_special_tokens=False):
+        return self.__tokenizer.decode(token_id, skip_special_tokens=skip_special_tokens)
+    
+    def destroy(self):
+        self.__rkllm_destroy(self.__rkllm_handle)
